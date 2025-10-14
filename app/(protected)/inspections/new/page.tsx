bf158ed6ca9ef505e127d2b6c3fab22830bac2c6
andre wijaya <andrewijaya988@gmail.com>
2025-10-14 13:41:20 +0700

    refactor: rm bg color

--- app/(protected)/inspections/new/page.tsx
+++ app/(protected)/inspections/new/page.tsx
@@ -157,8 +157,8 @@ export default function NewInspectionPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
-      <main className="max-w-4xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen">
+      <main className="max-w-4xl mx-auto py-6">
         <div className="mb-6">
           <BackButton />
 

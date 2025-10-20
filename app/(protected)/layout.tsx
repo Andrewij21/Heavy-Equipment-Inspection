1cc7f709c5f6ef4f6388b15175170f857ae86a38
andre wijaya <andrewijaya988@gmail.com>
2025-10-20 07:30:49 +0700

    feat: add loading animation

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -17,8 +17,12 @@ export default function ProtectedLayout({
       router.replace("/login");
     }
   }, [user, loading, router]);
-  if (loading && !user) {
-    return <div>Loading...</div>;
+  if (loading || !user) {
+    return (
+      <div className="flex h-screen items-center justify-center bg-gray-100">
+        <div className="h-16 w-16 animate-spin rounded-full border-b-4 border-t-4 border-blue-600"></div>
+      </div>
+    );
   }
 
   return <AppLayout>{children}</AppLayout>;
